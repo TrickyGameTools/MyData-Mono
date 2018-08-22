43fe6ceb02972ada885cbfbad7e6cd23ad2862d0
Tricky1975 <facebook@tbbs.nl>
2018-08-22 10:36:54 +0200

    File selection prior to actual running

--- MyData/Program.cs
+++ MyData/Program.cs
@@ -5,14 +5,33 @@ namespace MyData
 {
     class MainClass
     {
+        static public string filename;
+
+        static bool ChooseTheFile(Window w){
+            FileChooserDialog fcd = new FileChooserDialog("Choose database", w, FileChooserAction.Open,"Select", ResponseType.Accept, "Cancel", ResponseType.Close);
+            fcd.SelectMultiple = false;
+            var r = fcd.Run(); // This opens the window and waits for the response
+            bool alright = false;
+            if (r == (int)ResponseType.Accept)
+            {
+                filename = fcd.Filename;
+                alright = true;
+            }
+            fcd.Destroy(); // The dialog does not automatically close when clicking the buttons, so you have to manually close it with this
+            return alright;
+        }
+
         public static void Main(string[] args)
         {
             Application.Init();
             MainWindow win = new MainWindow();
             win.Title = "MyData - Coded by Tricky";
             win.Resize(1000, 800);
-            win.Show();
-            Application.Run();
+            if (ChooseTheFile(win))
+            {
+                win.Show();
+                Application.Run(); 
+            }
         }
     }
 }
