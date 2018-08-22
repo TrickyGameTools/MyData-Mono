c9f7b69dffc5c22d5ce5c57b08c94706b8c3b550
Tricky1975 <facebook@tbbs.nl>
2018-08-22 08:15:14 +0200

    Temp setip

--- MyData/DataBase.cs
+++ MyData/DataBase.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace MyData
 {
     public class DataBase
     {
+        public SortedDictionary<string, string> recitem = new SortedDictionary<string,string>(); // temp line... Something more accurate may come later.
         public DataBase()
         {
+
         }
     }
 }
